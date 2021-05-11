71c4ae6ad4429142063b2b63b73b705294343ed4
nbr41to <nbr.41to@gmail.com>
2021-05-11 16:43:55 +0900

    .

---
+++ practice-03.js
@@ -0,0 +1,20 @@
+/**
+ * # Practice01
+ *
+ */
+
+/* ↓ここの間にコードを書く↓ */
+
+
+
+/* ↑ここの間にコードを書く↑ */
+
+const famiry = ['叔母', '母', '姉', '妹'];
+
+famiry.push('父');
+famiry.shift('父');
+
+console.log(famiry);
+
+
+const;
