2f1692a8fdc07dd29ce58182cbb4be779a4b9028
nbr41to <nbr.41to@gmail.com>
2021-05-11 17:24:17 +0900

    .

---
+++ practice/practice-02.js
@@ -0,0 +1,16 @@
+/**
+ * # Practice02
+ * Consoleに
+ *  I'm busy.
+ * と表示させる 
+ */
+
+let status = 'free';
+
+/* ↓ここの間にコードを書く↓ */
+
+
+
+/* ↑ここの間にコードを書く↑ */
+
+console.log(`I'm ${status}.`);
---
+++ practice/practice-03.js
@@ -0,0 +1,29 @@
+/**
+ * # Practice01
+ * Consoleに以下を列挙
+ * 伊藤 一郎
+ * 伊藤 二郎
+ * 伊藤 三郎
+ * 伊藤 四郎
+ */
+
+const firstName = "郎";
+const famiryName = "伊藤";
+const kansuuji = ["一", "二", "三", "四"];
+
+const callItoFamiry = () => {
+  /* ↓ここの間にコードを書く↓ */
+
+
+
+  /* ↑ここの間にコードを書く↑ */
+};
+
+callItoFamiry();;
+
+
+/* Console */
+// 大河内 一郎
+// 大河内 二郎
+// 大河内 三郎
+// 大河内 四郎
