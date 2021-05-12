a5369ad3854de94a8c58f223bab3bdee6d788116
nbr41to <nbr.41to@gmail.com>
2021-05-12 13:22:29 +0900

    Update practice-00 copy.js

--- practice/practice-00 copy.js
+++ practice/practice-00 copy.js
@@ -4,12 +4,15 @@
  */
 
 const data = [
-  { id: 123, name: 'suzuki', is }
+  { id: '123', name: 'Suzuki', age: 12, isSuperman: false },
+  { id: '124', name: 'Tanaka', age: 20, isSuperman: true },
+  { id: '125', name: 'Katherine', age: 39, isSuperman: false },
+  { id: '126', name: 'Maeda', age: 28, isSuperman: false },
 ];
 
 
 /* ↓ここの間にコードを書く↓ */
-
+const;
 
 
 /* ↑ここの間にコードを書く↑ */
