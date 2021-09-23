a017f5e4ab3fbd01bdf8e4618f01376da551561c
nbr41to <nbr.41to@gmail.com>
2021-09-24 00:05:54 +0900

    .

---
+++ src/practice/ex0X.js
@@ -0,0 +1,43 @@
+/**
+ * Practice.02
+ * それぞれの支持に従った内容をConsoleに表示してください
+ */
+
+(() => {
+  console.log('===== 1 =====');
+  // 次のtextから半角と全角のスペースを削除したものを表示
+  const text =
+    '今日 は僕のマッ マの誕生日。　妹の妹子 もちょうど派遣 先から戻ってきた　ようだ 。少し恥ず　かしいけ ど日頃の"感 謝"を伝え　るのだ！';
+  /* ↓ここより下にコードを書く↓ */
+})();
+
+(() => {
+  console.log('===== 2 =====');
+  // 次のファイル名の配列から拡張子を取り除いたものを表示
+  const files = ['index.html', 'index.js', 'index.css', 'index.png'];
+  /* ↓ここより下にコードを書く↓ */
+})();
+
+(() => {
+  console.log('===== 3 =====');
+  // 次の文字列から「晴れています。」を一つ削除したものを表示
+  const text = '今日は晴れています。晴れています。38度です。';
+  /* ↓ここより下にコードを書く↓ */
+  console.log(text); // 今日は晴れています。が表示される
+})();
+
+(() => {
+  // 次のlistからランダムに一人選ばれ,その人がSupermanだった場合のみConsoleにnameを表示してください.
+  const list = [
+    { name: 'Suzuki', isSuperman: false },
+    { name: 'Tanaka', isSuperman: true },
+    { name: 'Katherine', isSuperman: false },
+    { name: 'Maeda', isSuperman: false },
+    { name: 'GojyoSato', isSuperman: true },
+  ];
+  /* ↓ここより下にコードを書く↓ */
+})();
+
+(() => {})();
+(() => {})();
+(() => {})();
