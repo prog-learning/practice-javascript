2670cad6805584d35b3c7a2d05ea9e4a4bf07463
nbr41to <nbr.41to@gmail.com>
2021-09-24 20:15:46 +0900

    .

--- src/practice/practice-03.js
+++ src/practice/practice-03.js
@@ -7,23 +7,13 @@
  * 伊藤 四郎
  */
 
-const firstName = "郎";
-const famiryName = "伊藤";
-const kansuuji = ["一", "二", "三", "四"];
+const firstName = '郎';
+const famiryName = '伊藤';
+const kansuuji = ['一', '二', '三', '四'];
 
 const callItoFamiry = () => {
   /* ↓ここの間にコードを書く↓ */
-
-
-
   /* ↑ここの間にコードを書く↑ */
 };
 
-callItoFamiry();;
-
-
-/* Console */
-// 大河内 一郎
-// 大河内 二郎
-// 大河内 三郎
-// 大河内 四郎
+callItoFamiry();
